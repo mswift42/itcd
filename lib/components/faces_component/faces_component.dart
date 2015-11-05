7770ec849325d9823d79e3ae55c764f5a6704d93
mswift42 <martin.haesler@gmail.com>
2015-11-05 18:38:16 +0000

    Load initial theme.

--- lib/components/faces_component/faces_component.dart
+++ lib/components/faces_component/faces_component.dart
@@ -2,11 +2,26 @@ library itcd.lib.components.faces_component.faces_component;
 
 import 'package:angular2/angular2.dart';
 import 'package:itcd/components/face_component/face_component.dart';
+import 'package:itcd/Face.dart';
 
 @Component(
-  selector: 'theme-faces',
-  templateUrl: 'faces-component.html',
-  directives: const [NgFor, FaceComponent]
-)
+    selector: 'theme-faces',
+    templateUrl: 'faces-component.html',
+    directives: const [NgFor, FaceComponent])
 
-class FacesComponent {}
+class FacesComponent {
+  List<Face> faces = [
+    new Face("mainbg", "#ffffff"),
+    new Face("mainfg", "#303030"),
+    new Face("builtin", "#000000"),
+    new Face("keyword", "#000000"),
+    new Face("string", "#000000"),
+    new Face("functionname", "#000000"),
+    new Face("variable", "#000000"),
+    new Face("type", "#000000"),
+    new Face("constant", "#000000"),
+    new Face("comment", "#606060"),
+    new Face("warning", "#ff0000"),
+    new Face("warning2", "#ff8800")
+  ];
+}
--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,11 @@
 import 'package:angular2/angular2.dart';
 import 'package:angular2/bootstrap.dart';
+import 'package:itcd/components/faces_component/faces_component.dart';
 
-@Component(selector: 'my-app', template: '<h1>My First Angular 2 App</h1>')
+@Component(
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1><theme-faces></theme-faces>',
+    directives: const [FacesComponent])
 class AppComponent {}
 
 main() {
