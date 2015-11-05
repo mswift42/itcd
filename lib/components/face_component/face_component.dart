4bb127d35ae66254089c2f7d933e32f66befde6f
mswift42 <martin.haesler@gmail.com>
2015-11-05 17:48:53 +0000

    Add face component.

---
+++ lib/components/face_component/face_component.dart
@@ -0,0 +1,12 @@
+library itcd.lib.components.face_component.face_component;
+
+import 'package:angular2/angular2.dart' show Component, NgStyle;
+import 'package:itcd/Face.dart';
+
+@Component(
+    selector: 'theme-face',
+    templateUrl: 'face-component.html',
+    directives: const [NgStyle])
+class FaceComponent {
+  Face face;
+}
