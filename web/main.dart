1dc99daa06ad76459bac392a43725ed04aeade5a
mswift42 <martin.haesler@gmail.com>
2015-11-05 18:39:37 +0000

    Remove header

--- web/main.dart
+++ web/main.dart
@@ -4,7 +4,7 @@ import 'package:itcd/components/faces_component/faces_component.dart';
 
 @Component(
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><theme-faces></theme-faces>',
+    template: '<theme-faces></theme-faces>',
     directives: const [FacesComponent])
 class AppComponent {}
 
