5fae6b9ffe85f22318c083e11e6b537b71075450
mswift42 <martin.haesler@gmail.com>
2015-11-06 18:02:59 +0000

    Add List facenames.

--- lib/Themes.dart
+++ lib/Themes.dart
@@ -3,4 +3,19 @@ library itcd.lib.Themes;
 import 'package:itcd/Theme.dart';
 import 'package:itcd/Face.dart';
 
-
+class Themes {
+  List<String> facenames = [
+    "mainbg",
+    "mainfg",
+    "builtin",
+    "keyword",
+    "string",
+    "functionname",
+    "variable",
+    "type",
+    "constant",
+    "comment",
+    "warning",
+    "warning2"
+  ];
+}
