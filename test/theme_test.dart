20f8a2c6ed10cf722b7fd456232908a4d9f18a61
mswift42 <martin.haesler@gmail.com>
2015-11-06 19:47:51 +0000

    Test initialisation of facemap.

--- test/theme_test.dart
+++ test/theme_test.dart
@@ -5,20 +5,26 @@ import 'package:test/test.dart';
 
 void main() {
   test('Theme gets initialised correctly', () {
-    var t1 = new Theme([ "#f5ebe1",
-    "#585858",
-    "#4a858c",
-    "#1a8591",
-    "#a9a9a9",
-    "#8c4a79",
-    "#476238",
-    "#bd745e",
-    "#ff1276",
-    "#ff4d12",
-    "#697024",
-    "#b3534b"]);
+    var t1 = new Theme([
+      "#f5ebe1", // mainbg
+      "#585858", // mainfg
+      "#4a858c", // keyword
+      "#1a8591", // builtin
+      "#b3534b", // string
+      "#bd745e", // functionname
+      "#476238", // variable
+      "#8c4a79", // type
+      "#697024", // constant
+      "#a9a9a9", // comment
+      "#ff1276", // warning
+      "#ff4d12"  // warning2
+]);
     expect(t1.facemap["mainbg"], "#f5ebe1");
     expect(t1.facemap["warning2"], "#ff4d12");
-
+    expect(t1.facemap["builtin"], "#1a8591");
+    expect(t1.facemap["functionname"], "#bd745e");
+    expect(t1.facemap["warning"], "#ff1276");
+    expect(t1.facemap["type"], "#8c4a79");
+    expect(t1.facemap["mainfg"], "#585858");
   });
 }
