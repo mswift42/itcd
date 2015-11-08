5a92543f3dab1ad92e9e66101514cc06c8ebfdbb
mswift42 <martin.haesler@gmail.com>
2015-11-08 08:50:29 +0000

    add preset theme

--- lib/Themeservice.dart
+++ lib/Themeservice.dart
@@ -1,22 +1,42 @@
-library itcd.lib.Themes;
+library itcd.lib.Themeservice;
 
 import 'package:itcd/Theme.dart';
 import 'package:itcd/Face.dart';
 
-Theme whitesand() {
-  var ws = new Theme([
-    "#f5ebe1", // mainbg
-    "#585858", // mainfg
-    "#4a858c", // keyword
-    "#1a8591", // builtin
-    "#b3534b", // string
-    "#bd745e", // functionname
-    "#476238", // variable
-    "#8c4a79", // type
-    "#697024", // constant
-    "#a9a9a9", // comment
-    "#ff1276", // warning
-    "#ff4d12"  // warning2
-  ]);
-  return ws.facemap;
+class ThemeService {
+  ThemeService();
+  Theme preset() {
+    return new Theme([
+      "#ffffff",
+      "#303030",
+      "#000000",
+      "#000000",
+      "#000000",
+      "#000000",
+      "#000000",
+      "#000000",
+      "#000000",
+      "#606060",
+      "#ff0000",
+      "#ff8800"
+    ]);
+  }
+
+  Theme whitesand() {
+    var ws = new Theme([
+      "#f5ebe1", // mainbg
+      "#585858", // mainfg
+      "#4a858c", // keyword
+      "#1a8591", // builtin
+      "#b3534b", // string
+      "#bd745e", // functionname
+      "#476238", // variable
+      "#8c4a79", // type
+      "#697024", // constant
+      "#a9a9a9", // comment
+      "#ff1276", // warning
+      "#ff4d12" // warning2
+    ]);
+    return ws;
+  }
 }
