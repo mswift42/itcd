6b97adef7cc7588beb0da2dbbfcdf62465feb6f1
mswift42 <martin.haesler@gmail.com>
2015-11-09 13:49:13 +0000

    Add static method whitesand to themes.

--- lib/Themes.dart
+++ lib/Themes.dart
@@ -1,27 +1,27 @@
-library itcd.lib.Themeservice;
+library itcd.lib.Themes;
 
 import 'package:itcd/Theme.dart';
-import 'package:itcd/Face.dart';
 
-class ThemeService {
-  ThemeService();
-  Theme preset() {
+ class Themes {
+  Themes();
+  static Theme preset() {
     return new Theme();
   }
-  Theme whitesand() {
+
+  static Theme whitesand() {
     var ws = new Theme();
-        ws.mainbg = "#f5ebe1";
-        ws.mainfg = "#585858";
-        ws.keyword = "#4a858c";
-        ws.builtin = "#1a8591";
-        ws.string = "#b3534b";
-        ws.functionname = "#bd745e";
-        ws.variable = "#476238";
-        ws.type = "#8c4a79";
-        ws.constant = "#697024";
-        ws.comment = "#a9a9a9";
-        ws.warning = "#ff1276";
-        ws.warning2 = "#ff4d12";
+    ws.mainbg = "#f5ebe1";
+    ws.mainfg = "#585858";
+    ws.keyword = "#4a858c";
+    ws.builtin = "#1a8591";
+    ws.string = "#b3534b";
+    ws.functionname = "#bd745e";
+    ws.variable = "#476238";
+    ws.type = "#8c4a79";
+    ws.constant = "#697024";
+    ws.comment = "#a9a9a9";
+    ws.warning = "#ff1276";
+    ws.warning2 = "#ff4d12";
     return ws;
   }
 }
