ff1517578d924ce8240815961ce5c20e22716ff2
mswift42 <martin.haesler@gmail.com>
2015-11-15 10:29:11 +0000

    Add changecolor method for themeservice

--- lib/Themeservice.dart
+++ lib/Themeservice.dart
@@ -7,11 +7,28 @@ import 'package:angular2/angular2.dart';
 @Injectable()
 class ThemeService {
   Theme theme;
+  List<String> facenames = [
+    "mainbg",
+    "mainfg",
+    "keyword",
+    "builtin",
+    "string",
+    "functionname",
+    "variable",
+    "type",
+    "constant",
+    "comment",
+    "warning",
+    "warning2"
+  ];
   ThemeService();
 
   Theme get() => theme;
 
   void setTheme(Theme theme) {
     this.theme = theme;
+  }
+  void changeColor(String name, String color) {
+    theme.facemap[name] = color;
   }
 }
