9d3a21560847a79aaedb2512e3f337036142aaf7
mswift42 <martin.haesler@gmail.com>
2015-11-16 15:29:17 +0000

    dartfmt file

--- lib/Theme.dart
+++ lib/Theme.dart
@@ -1,20 +1,19 @@
 library itcd.lib.Theme;
 
 class Theme {
-
-  Map <String,String> facemap = {
-    "mainbg" : "#ffffff",
-  "mainfg" : "#303030",
-    "keyword" : "#000000",
-    "builtin" : "#000000",
-    "string" : "#000000",
-    "functionname" : "#000000",
-    "variable" : "#000000",
-    "type" : "#000000",
-    "constant" : "#000000",
-    "comment" : "#606060",
-    "warning" : "#ff0000",
-    "warning2" : "#ff8800"
+  Map<String, String> facemap = {
+    "mainbg": "#ffffff",
+    "mainfg": "#303030",
+    "keyword": "#000000",
+    "builtin": "#000000",
+    "string": "#000000",
+    "functionname": "#000000",
+    "variable": "#000000",
+    "type": "#000000",
+    "constant": "#000000",
+    "comment": "#606060",
+    "warning": "#ff0000",
+    "warning2": "#ff8800"
   };
 
   Theme();
