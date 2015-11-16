88b52a257ec4519f34a29dc855c913706a1c0e46
mswift42 <martin.haesler@gmail.com>
2015-11-16 15:32:11 +0000

    Use service themeservice

--- lib/components/faces_component/faces_component.dart
+++ lib/components/faces_component/faces_component.dart
@@ -3,27 +3,22 @@ library itcd.lib.components.faces_component.faces_component;
 import 'package:angular2/angular2.dart';
 import 'package:itcd/components/face_component/face_component.dart';
 import 'package:itcd/Face.dart';
+import 'package:itcd/Themeservice.dart';
+import 'package:itcd/Theme.dart';
 import 'package:itcd/Themes.dart';
 
 @Component(
     selector: 'theme-faces',
     templateUrl: 'faces-component.html',
-    directives: const [NgFor, FaceComponent])
+    directives: const [NgFor, FaceComponent],
+    providers: const [ThemeService])
 
 class FacesComponent {
-  FacesComponent(ThemeService ThemeService);
-  List<Face> faces = [
-    new Face("mainbg", "#ffffff"),
-    new Face("mainfg", "#303030"),
-    new Face("builtin", "#000000"),
-    new Face("keyword", "#000000"),
-    new Face("string", "#000000"),
-    new Face("functionname", "#000000"),
-    new Face("variable", "#000000"),
-    new Face("type", "#000000"),
-    new Face("constant", "#000000"),
-    new Face("comment", "#606060"),
-    new Face("warning", "#ff0000"),
-    new Face("warning2", "#ff8800")
-  ];
+  Map<String, String> theme;
+  List<String> facenames;
+  FacesComponent(ThemeService ts) {
+    ts.setTheme(Themes.preset());
+    theme = ts.get().facemap;
+    facenames = ts.facenames;
+  }
 }
