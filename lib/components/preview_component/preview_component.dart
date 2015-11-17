6fa07470ef4d37989da2ef771fa3c36343a289c6
mswift42 <martin.haesler@gmail.com>
2015-11-17 16:39:41 +0000

    Add PreviewComponent class.

--- lib/components/preview_component/preview_component.dart
+++ lib/components/preview_component/preview_component.dart
@@ -1,3 +1,19 @@
 library itcd.lib.components.preview_component.preview_component;
 
-import 'package:angular2/angular2.dart';
+import 'package:angular2/angular2.dart' show Component, NgStyle;
+import 'package:itcd/Theme.dart';
+import 'package:itcd/Themeservice.dart';
+import 'package:itcd/Themes.dart';
+
+@Component(
+    selector: 'preview',
+    templateUrl: 'preview-component.html',
+    directives: const [NgStyle])
+class PreviewComponent {
+  String mainbg;
+  String keyword;
+  PreviewComponent(ThemeService ts) {
+    mainbg = ts.theme.facemap["mainbg"];
+    keyword = ts.theme.facemap["keyword"];
+  }
+}
