2400cc89a9373da5647d043e6a28d023060937d4
mswift42 <martin.haesler@gmail.com>
2015-11-17 16:40:56 +0000

    Display preview .

--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,15 @@
 import 'package:angular2/angular2.dart';
 import 'package:angular2/bootstrap.dart';
 import 'package:itcd/components/faces_component/faces_component.dart';
+import 'package:itcd/components/preview_component/preview_component.dart';
 import 'package:itcd/Themeservice.dart';
 
 @Component(
     selector: 'my-app',
-    template: '<theme-faces></theme-faces>',
-    directives: const [FacesComponent])
+    template: '''<div class="row"><div class="col-md-4">
+        <theme-faces></theme-faces></div><div class="col-md-4">
+        <preview></preview></div></div>''',
+    directives: const [FacesComponent, PreviewComponent])
 class AppComponent {}
 
 main() {
